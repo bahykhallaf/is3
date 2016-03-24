d3ba08f9027b7d692b1b3a719b2e19353417cc93
KyrillosMounier <KyrillosMounier@github.com>
2016-03-24 14:44:27 +0200

    added method

--- src/asd/asd1.java
+++ src/asd/asd1.java
@@ -4,7 +4,12 @@ class asd1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello worled");
 
+	}
+	public static void welcom()
+	{
+		System.out.println("hiiiiiiii");
 	}
 
 }
