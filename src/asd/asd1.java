1cf2846f9b9a060050908e529ee34ac0e6ed4de7
KyrillosMounier <KyrillosMounier@github.com>
2016-03-24 14:52:48 +0200

    Revert "error"
    This reverts commit f07184a3c9b0f9e42f272852d966e923ee58d643.

--- src/asd/asd1.java
+++ src/asd/asd1.java
@@ -5,13 +5,11 @@ class asd1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello worled");
-		welcom();
 
 	}
 	public static void welcom()
 	{
 		System.out.println("hiiiiiiii");
-		int x;
 	}
 
 }
